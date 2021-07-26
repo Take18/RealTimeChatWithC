b435c8b5c914ff9f739c050aff165b8d228ace57
takerin <takerin0518@gmail.com>
2021-07-27 00:05:26 +0900

    いらんコメント削除 remove unnecessary comments.

--- server.c
+++ server.c
@@ -1,10 +1,3 @@
-/*
- * server.c
- *   クライアントからの接続要求を受け付けるサーバープログラム。
- *
- *   クライアントから送られてきた文字列を大文字に変換して送り返す。
- */
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -50,37 +43,22 @@ int main() {
 	int msg_num = 0;
 
 
-	/*
-	 *  ストリーム型ソケット作る．
-	 */
 	if ((fd1 = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
 	  perror("socket");
 	  exit(1);
 	}
 
-	/*
-	 * saddrの中身を0にしておかないと、bind()でエラーが起こることがある
-	 */
 	bzero((char *)&saddr, sizeof(saddr));
 
-	/*
-	 * ソケットの名前を入れておく
-	 */
 	saddr.sin_family      = PF_INET;
 	saddr.sin_addr.s_addr = INADDR_ANY;
 	saddr.sin_port        = htons(PORT);
 
-	/*
-	 * ソケットにアドレスをバインドする。
-	 */
 	if (bind(fd1, (struct sockaddr *)&saddr, sizeof(saddr)) < 0){
 	  perror("bind");
 	  exit(1);
 	}
 
-	/*
-	 * listenをソケットに対して発行する
-	 */
 	if (listen(fd1, 1) < 0) {
 	  perror("listen");
 	  exit(1);
@@ -88,12 +66,6 @@ int main() {
 
 	len = sizeof(caddr);
 	while(1) {
-		/*
-		* accept()により、クライアントからの接続要求を受け付ける。
-		* 成功すると、クライアントと接続されたソケットのディスクリプタが
-		* fd2に返される。このfd2を通して通信が可能となる。
-		* fd1は必要なくなるので、close()で閉じる。
-		*/
 		if ((fd2 = accept(fd1, (struct sockaddr *)&caddr, &len)) < 0) {
 			perror("accept");
 			exit(1);
